2715f868d7bddea9cc73da18f4e5bd52c209b9d6
github username <github email address>
2017-08-09 09:04:02 -0400

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,8 @@
+#Define dog
+class Dog
+end
+
+#Make 3 dogs
+fido = Dog.new
+snoopy = Dog.new
+lassie = Dog.new
---
+++ lib/person.rb
@@ -0,0 +1,7 @@
+#Define a person
+class Person
+end
+
+#Make 2 people
+adele_goldberg = Person.new
+alan_kay = Person.new
